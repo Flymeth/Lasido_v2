41b78aefa082847ce5ee0a3a89008913403a504d
Flymeth <flymeth.yt@gmail.com>
2023-12-30 11:24:04 +0100

    Added a call to action message (yes)

--- dist/events/message.js
+++ dist/events/message.js
@@ -6,6 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const EventClass_1 = __importDefault(require("../types/EventClass"));
 const settings_1 = require("../utils/settings");
 const player_1 = require("../commands/platines/player");
+const otherMusicBotIds = [
+    "760027263046909992",
+    "184405311681986560"
+];
+let timesAskedForUsingMe = 0;
 class BotMessageEvent extends EventClass_1.default {
     constructor(lasido) {
         super(lasido, "messageCreate");
@@ -13,6 +18,14 @@ class BotMessageEvent extends EventClass_1.default {
     async handle(msg, ...args) {
         if (!msg.guildId)
             return;
+        if (msg.interaction && otherMusicBotIds.includes(msg.author.id)) {
+            if (!timesAskedForUsingMe) {
+                msg.channel.send({
+                    content: "Hey! Don't forget I'm here too 🥺...",
+                }).then(m => setTimeout(() => m.delete(), 5000)).catch(() => undefined);
+            }
+            timesAskedForUsingMe = (timesAskedForUsingMe + 1) % Math.max(this.lasido.guilds.cache.size, 5);
+        }
         const { guildId, channelId } = msg;
         if (await (0, settings_1.settingsGenerated)(guildId)) {
             const { player } = (await (0, settings_1.getSettings)(guildId)).settings;
--- src/events/message.ts
+++ src/events/message.ts
@@ -1,9 +1,14 @@
-import { BaseGuildTextChannel, Message } from "discord.js";
+import { Message } from "discord.js";
 import { Lasido } from "../_main";
 import BotEvent from "../types/EventClass";
 import { getSettings, settingsGenerated, updateSettings } from "../utils/settings";
 import { stickPlayersFooterMessage } from "../commands/platines/player";
 
+const otherMusicBotIds = [
+    "760027263046909992",
+    "184405311681986560"
+]
+let timesAskedForUsingMe = 0
 export default class BotMessageEvent extends BotEvent {
     constructor(lasido: Lasido) {
         super(lasido, "messageCreate")
@@ -11,6 +16,15 @@ export default class BotMessageEvent extends BotEvent {
 
     async handle(msg: Message, ...args: any[]): Promise<any> {
         if(!msg.guildId) return
+        
+        if(msg.interaction && otherMusicBotIds.includes(msg.author.id)) {
+            if(!timesAskedForUsingMe) {
+                msg.channel.send({
+                    content: "Hey! Don't forget I'm here too 🥺...",
+                }).then(m => setTimeout(() => m.delete(), 5000)).catch(() => undefined)
+            }
+            timesAskedForUsingMe= (timesAskedForUsingMe + 1) % Math.max(this.lasido.guilds.cache.size, 5)
+        }
 
         // Edit player message (if sticky)
         const { guildId, channelId } = msg
